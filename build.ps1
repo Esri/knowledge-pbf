92ab4629166de99adc0f28f028a3640db19fa1eb
mand6404 <mpurohit@esri.com>
2021-12-02 13:28:38 -0800

    Updating comments format in the build file, and Java execution.

--- build.ps1
+++ build.ps1
@@ -1,18 +1,18 @@
 
-/* Copyright 2021 Esri
- *
- * Licensed under the Apache License Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+<# Copyright 2021 Esri
+ #
+ # Licensed under the Apache License Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #>
  
 $compiler_path = "bin\protoc.exe"
 $proto_folder = Get-Item ".\proto"
@@ -350,7 +350,7 @@ try
 
 	# Generate Java code last because it's replacing `optimize_for` inside the Proto files.
 	# The `Generate-Java` function brings the files back to their original state but keep this last just in case.
-	Generate-Java $java_out
+	# Generate-Java $java_out
 
 	$protoc_ver = Write-Compiler-Version
 
